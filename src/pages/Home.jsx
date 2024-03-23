34c57a860b4f56766edf46683682bd2848d0a92a
Rita Martins <mazedoodle@gmail.com>
2024-03-23 11:19:05 +0000

    added tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { getAllArtworks } from '../api/artwork.api';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
 
 function Home() {
   const [artworks, setArtworks] = useState([]);
@@ -20,7 +21,30 @@ function Home() {
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-right">
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            'Find',
+            3000, // wait 1s before replacing "Mice" with "Hamsters"
+            'Create',
+            3000,
+            'Commission',
+            3000,
+            'Explore',
+            3000,
+            'Sell',
+            3000,
+          ]}
+          wrapper="span"
+          speed={50}
+          cursor={false}
+          // style={{ fontSize: '2em', display: 'inline-block' }}
+          repeat={Infinity}
+        />{' '}
+        Illustrations
+      </h1>
+      {/* <h1>Home</h1> */}
       {artworks.map(artwork => {
         return (
           <Link
